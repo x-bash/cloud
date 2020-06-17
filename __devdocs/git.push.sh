bce32cf794d9a74678cb6870a92a8328ba98735e
edwinjhlee <edwin.jh.lee@gmail.com>
2020-06-17 21:08:48 +0800

    git push all

---
+++ __devdocs/git.push.sh
@@ -0,0 +1,2 @@
+git remote add gitee git@gitee.com:x-bash/x-bash.gitee.io.git 2>/dev/null
+git push gitee
--- __devdocs/git.sync.sh
+++
@@ -1,2 +0,0 @@
-git remote add gitee git@gitee.com:x-bash/x-bash.gitee.io.git
-git push gitee
