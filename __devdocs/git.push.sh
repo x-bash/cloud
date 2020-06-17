b810c9c1d443cf51eae0d246aa2246ddf640c683
edwinjhlee <edwin.jh.lee@gmail.com>
2020-06-17 21:10:49 +0800

    fix script

--- __devdocs/git.push.sh
+++ __devdocs/git.push.sh
@@ -1,3 +1,4 @@
-git remote add gitee git@gitee.com:x-bash/cloud.git 2>/dev/null
+git remote remove gitee 2>/dev/null
+git remote add gitee git@gitee.com:x-bash/cloud.git
 git push gitee
 git push origin
